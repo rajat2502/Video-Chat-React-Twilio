774dc1d665829144089bc5d0ba1131daae24c675
Rajat Verma <rajatverma5885045@gmail.com>
2020-04-27 01:26:19 +0530

    prepare to deploy

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-const config = require('./config');
-const express = require('express');
-const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
-const { chatToken, videoToken, voiceToken } = require('./tokens');
+const config = require("./config");
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const pino = require("express-pino-logger")();
+const { chatToken, videoToken, voiceToken } = require("./tokens");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,7 +11,7 @@ app.use(bodyParser.json());
 app.use(pino);
 
 const sendTokenResponse = (token, res) => {
-  res.set('Content-Type', 'application/json');
+  res.set("Content-Type", "application/json");
   res.send(
     JSON.stringify({
       token: token.toJwt()
@@ -18,50 +19,66 @@ const sendTokenResponse = (token, res) => {
   );
 };
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
+app.get("/api/greeting", (req, res) => {
+  const name = req.query.name || "World";
+  res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.get('/chat/token', (req, res) => {
+app.get("/chat/token", (req, res) => {
   const identity = req.query.identity;
   const token = chatToken(identity, config);
   sendTokenResponse(token, res);
 });
 
-app.post('/chat/token', (req, res) => {
+app.post("/chat/token", (req, res) => {
   const identity = req.body.identity;
   const token = chatToken(identity, config);
   sendTokenResponse(token, res);
 });
 
-app.get('/video/token', (req, res) => {
+app.get("/video/token", (req, res) => {
   const identity = req.query.identity;
   const room = req.query.room;
   const token = videoToken(identity, room, config);
   sendTokenResponse(token, res);
 });
 
-app.post('/video/token', (req, res) => {
+app.post("/video/token", (req, res) => {
   const identity = req.body.identity;
   const room = req.body.room;
   const token = videoToken(identity, room, config);
   sendTokenResponse(token, res);
 });
 
-app.get('/voice/token', (req, res) => {
+app.get("/voice/token", (req, res) => {
   const identity = req.body.identity;
   const token = voiceToken(identity, config);
   sendTokenResponse(token, res);
 });
 
-app.post('/voice/token', (req, res) => {
+app.post("/voice/token", (req, res) => {
   const identity = req.body.identity;
   const token = voiceToken(identity, config);
   sendTokenResponse(token, res);
 });
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, "../build")));
+
+//production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../build")));
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname, "../build/index.html")));
+  });
+} else {
+  //build mode
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../public/index.html"));
+  });
+}
 
 app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
+  console.log("Express server is running on localhost:3001")
 );
